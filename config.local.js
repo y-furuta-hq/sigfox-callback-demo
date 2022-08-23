c9c5fad83233bab69b0dfc2f2f9c368d61b3c550
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-23 19:27:53 +0900

    added config.local.js and modified don't use process env

---
+++ config.local.js
@@ -0,0 +1,5 @@
+// Local environment settings
+module.exports = {
+    DATABASE_URL: 'postgres://wpddybbgshwvui:a87b23bab33fc8d199121db46af83cf7f09ac2e8ac9b123347c589e04b77628b@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d6a38otk3ah857',
+    PORT: '8000'
+};
