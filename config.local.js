23108cce9baa61feece344c7c5b26197c38d47c1
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-23 19:36:27 +0900

    modified config.local.js

--- config.local.js
+++ config.local.js
@@ -1,5 +1,5 @@
 // Local environment settings
 module.exports = {
-    DATABASE_URL: 'postgres://wpddybbgshwvui:a87b23bab33fc8d199121db46af83cf7f09ac2e8ac9b123347c589e04b77628b@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d6a38otk3ah857',
+    DATABASE_URL: 'postgres://fzzkiuilytskyd:debc5630936bf4b9dca8b7c9db45d540bf6729e360ab1973e5dab0268abd96ee@ec2-44-209-186-51.compute-1.amazonaws.com:5432/d2q57gtubg9guu',
     PORT: '8000'
 };
