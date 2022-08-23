b1f3e5604225ef33c3073c812a37e6cf66870b04
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-23 16:56:33 +0900

    created env file

---
+++ config.local.js
@@ -0,0 +1,5 @@
+// Local environment settings
+module.exports = {
+    DATABASE_URL: 'postgres://wpddybbgshwvui:a87b23bab33fc8d199121db46af83cf7f09ac2e8ac9b123347c589e04b77628b@ec2-3-223-242-224.compute-1.amazonaws.com',
+    PORT: '5432'
+};
