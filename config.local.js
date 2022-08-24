fc6692d1c837320d85449673b1e49126dbf7eff1
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-24 16:53:14 +0900

    modified local env

--- config.local.js
+++ config.local.js
@@ -1,5 +1,5 @@
 // Local environment settings
 module.exports = {
-    DATABASE_URL: 'postgres://fzzkiuilytskyd:debc5630936bf4b9dca8b7c9db45d540bf6729e360ab1973e5dab0268abd96ee@ec2-44-209-186-51.compute-1.amazonaws.com:5432/d2q57gtubg9guu',
+    DATABASE_URL: 'postgres://fz......:de.....@ec2.......amazonaws.com:5432/d2...',
     PORT: '8000'
 };
