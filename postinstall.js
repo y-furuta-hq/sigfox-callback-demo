3fa9b6109d31a9b524b903c4dcd2f56954f9c9b2
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-24 15:38:13 +0900

    modified ssl

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,9 @@ require('./loadConfig.js');
 const { Pool, Client } = require('pg');
 const client = new Client({
   connectionString:process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 const sql = require('fs').readFileSync('./init.sql', {encoding:'utf-8'});
 client.connect(function(err){
