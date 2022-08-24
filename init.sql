d5088305379cecd77b39f06788c876640b6e5bc8
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-24 20:36:39 +0900

    deleted drop type of sql

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 drop table if exists sigfox_data;
-drop type if exists callback_type;
+-- drop type if exists callback_type;
 CREATE TYPE callback_type AS ENUM ('data/uplink', 'data/downlink', 'service/geoloc');
 
 create table if not exists callbacks(
