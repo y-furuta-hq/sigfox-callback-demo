3b4c65f9f18e19afbe5e3662829f9b1ee2dca866
y-furuta-hq <y.furuta@happy-quality.jp>
2022-08-24 20:50:54 +0900

    sql table name changed to sigfox_data

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@
 -- drop type if exists callback_type;
 -- CREATE TYPE IF NOT EXISTS callback_type AS ENUM ('data/uplink', 'data/downlink', 'service/geoloc');
 
-create table if not exists callbacks(
+create table if not exists sigfox_data(
   id serial primary key,
   date timestamp not null,
   type callback_type not null,
